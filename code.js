d6221f75d3f907f284d7998336829a52d82036a2
dev-alihasan <infomeorsendmehelp@protonmail.com>
2023-11-09 23:04:45 +0600

    clean js code

---
+++ code.js
@@ -0,0 +1,25 @@
+// use clean names
+
+// not good 
+let a = 21; //what's a ?
+
+// good code
+let age = 21; //Describes what age means
+
+
+/*-------------------------------------------------------*/
+// short and focused function
+
+// Not good:
+function startCarAndDriveToDestination() {
+    // too many things happening
+}
+
+// Good:
+function startCar() {
+    // Does one clear thing
+}
+
+function driveToDestination() {
+    // Does another clear thing
+}
